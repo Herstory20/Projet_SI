703a1acaf1a93bacc105a5e9fb94b0805a462524
Herstory20 <bzhong@insa-toulouse.fr>
2023-04-21 10:50:42 +0200

    Alu et BR

---
+++ processeur_si.srcs/sources_1/new/ALU.vhd
@@ -0,0 +1,71 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 21.04.2023 08:53:53
+-- Design Name: 
+-- Module Name: ALU - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+library UNISIM;
+use UNISIM.VComponents.all;
+
+entity ALU is
+    Port ( A : in STD_LOGIC_VECTOR (7 downto 0);
+           B : in STD_LOGIC_VECTOR (7 downto 0);
+           S : out STD_LOGIC_VECTOR (7 downto 0);
+           N : out STD_LOGIC;
+           O : out STD_LOGIC;
+           Z : out STD_LOGIC;
+           C : out STD_LOGIC;
+           Ctrl_Alu : in STD_LOGIC_VECTOR (2 downto 0));
+end ALU;
+
+architecture Behavioral of ALU is
+
+    signal Temp_res : STD_LOGIC_VECTOR (8 downto 0) := (others => '0');
+    signal Aux : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
+    
+begin
+
+    S <= Aux ;
+    
+    process
+    begin
+        case Ctrl_Alu is
+            when x"01" => Temp_res <= ('0'+A) + ('0'+B);   
+            when x"10" => Temp_res <= ('0'+A) * ('0'+B);
+            when x"11" => Temp_res <= ('0'+A) - ('0'+B);
+            --when x"100" => Temp_res <= ('0'+A) / ('0'+B);
+            when others => Aux <= x"00000000" ;
+        end case;
+        Aux <= Temp_res(7 downto 0);
+        if Aux < x"00000000" then N<= '1'; else N<='0'; end if;
+        if Temp_res(8)='1' then O<= '1'; else O<='0'; end if;
+        if Aux = x"00000000" then Z <='1'; else Z<='0'; end if;
+        if Temp_res(8)='1' then C <= '1'; else C<='0'; end if;
+    end process;
+
+end Behavioral;
---
+++ processeur_si.srcs/sources_1/new/BR.vhd
@@ -0,0 +1,66 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 21.04.2023 10:16:01
+-- Design Name: 
+-- Module Name: BR - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+library UNISIM;
+use UNISIM.VComponents.all;
+
+entity BR is
+    Port ( AddrA : in STD_LOGIC_VECTOR (3 downto 0);
+           AddrB : in STD_LOGIC_VECTOR (3 downto 0);
+           AddrW : in STD_LOGIC_VECTOR (3 downto 0);
+           W : in STD_LOGIC;
+           DATA : in STD_LOGIC_VECTOR (7 downto 0);
+           RST : in STD_LOGIC;
+           CLK : in STD_LOGIC;
+           QA : out STD_LOGIC_VECTOR (7 downto 0);
+           QB : out STD_LOGIC_VECTOR (7 downto 0));
+end BR;
+
+architecture Behavioral of BR is
+    type reg_array is array (0 to 15) of STD_LOGIC_VECTOR (7 downto 0);
+    signal bancreg : reg_array := (others => (others => '0'));
+begin
+
+    process 
+        begin
+            if (RST = '1') then
+             bancreg <= (others => (others => '0'));
+            else
+                wait until CLK'event and CLK='1';
+                if W ='1' then bancreg(to_integer(unsigned(AddrW))) <= DATA;
+                end if;
+            end if;
+    end process;
+    
+    QA <= bancreg(to_integer(unsigned(AddrA)));
+    QB <= bancreg(to_integer(unsigned(AddrB)));
+    
+
+end Behavioral;
